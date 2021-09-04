a02b2a0ae2f7366c43e743bb2dc8f27f505447d4
Gouri M Menon <83968970+Gourimmenon@users.noreply.github.com>
2021-09-04 06:27:36 -0700

    Create whileloop pattern.py

---
+++ whileloop pattern.py
@@ -0,0 +1,4 @@
+i=1 
+while i<=5:                                             #loop condition
+    print('*'*i)                                        #Print '*' i times
+    i=i+1                                               #increment value of i
