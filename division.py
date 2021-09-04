ebb42ef74b7affe612cff080b8fa762baf0979ee
Gouri M Menon <83968970+Gourimmenon@users.noreply.github.com>
2021-09-04 06:07:42 -0700

    Create division.py

---
+++ division.py
@@ -0,0 +1,9 @@
+a = float(input("enter first number:"))
+b = float(input("enter second number:"))
+x = a/b
+
+print(type(a))
+print(type(b))
+print(type(x))
+
+print("value after division is:%s"%(x))
