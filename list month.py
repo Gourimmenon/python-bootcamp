8eac53474bd4d98fac17f7bad005d12f8faffe57
Gouri M Menon <83968970+Gourimmenon@users.noreply.github.com>
2021-09-04 07:07:39 -0700

    Create list month.py

---
+++ list month.py
@@ -0,0 +1,5 @@
+months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+for i in range(0,12):                                           #for loop from i=0 to 12
+    print(months[i], end=" ")                                   #print without new line
+  
