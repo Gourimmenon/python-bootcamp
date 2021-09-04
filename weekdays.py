2e3b13c9da329f736f809af743095af75848d313
Gouri M Menon <83968970+Gourimmenon@users.noreply.github.com>
2021-09-04 06:11:24 -0700

    Create weekdays.py

---
+++ weekdays.py
@@ -0,0 +1,18 @@
+x= int(input("enter a number from 1 to 7:"))       #stores integer value to x
+
+if x==1:
+    print("Sunday")
+elif x==2:
+    print("Monday")
+elif x==3:
+    print("Tuesday")
+elif x==4:
+    print("Wednesday")
+elif x==5:
+    print("Thursday")
+elif x==6:
+    print("Friday")
+elif x==7:
+    print("Saturday")
+else:
+    print("Invalid input, Try again")
