a927ddcff1f2947350fa67802e74881d9b0b30ca
Gouri M Menon <83968970+Gourimmenon@users.noreply.github.com>
2021-09-04 07:13:33 -0700

    Create revlist.py

---
+++ revlist.py
@@ -0,0 +1,10 @@
+l = [2,5,3,6,4,9,8,6,7,4,5,2,16,56,6]                #define list
+
+def rev(list):                                       #function declaration
+    r = list [::-1]
+    return r
+    
+
+
+x = rev(l)                                           #function calling
+print("The reverse list is:",x)    
