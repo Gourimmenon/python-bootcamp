e159e14beb80441a12b3192864e054f117c13081
Gouri M Menon <83968970+Gourimmenon@users.noreply.github.com>
2021-09-04 06:29:20 -0700

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+a = 0                                                   #assign initial value to a,b and i
+b = 1
+i=1                                                     
+while i<=50:                                            #loop condition
+    print(a)
+    r=a+b 
+    a=b 
+    b=r
+    i=i+1                                               #updation
