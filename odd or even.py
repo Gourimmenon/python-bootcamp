8be61e838dda085b723b15d525324cbc00e81581
Gouri M Menon <83968970+Gourimmenon@users.noreply.github.com>
2021-09-04 06:11:56 -0700

    Create odd or even.py

---
+++ odd or even.py
@@ -0,0 +1,6 @@
+x = int(input("enter a positive number:"))          #stores integer value to x
+
+if x%2==0:                                          #x is even if it is divisible by 2
+    print("Even")
+else:                                               #x is odd if not divisible by 2
+    print("Odd")
